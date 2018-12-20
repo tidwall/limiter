fcddc63bb5219039fc9924fe85e25bf67669cfd4
tidwall <joshbaker77@gmail.com>
2018-12-19 19:01:58 -0700

    Use buffered channel #1

--- limiter.go
+++ limiter.go
@@ -1,37 +1,20 @@
 package limiter
 
-import "sync"
-
 // Limiter is for limiting the number of concurrent operations. This
-type Limiter struct {
-	limit int
-	cond  *sync.Cond
-	count int
-}
+type Limiter struct{ sem chan struct{} }
 
 // New returns a new Limiter. The limit param is the maximum number of
 // concurrent operations.
 func New(limit int) *Limiter {
-	return &Limiter{
-		limit: limit,
-		cond:  sync.NewCond(&sync.Mutex{}),
-	}
+	return &Limiter{make(chan struct{}, limit)}
 }
 
 // Begin an operation.
 func (l *Limiter) Begin() {
-	l.cond.L.Lock()
-	for l.count == l.limit {
-		l.cond.Wait()
-	}
-	l.count++
-	l.cond.L.Unlock()
+	l.sem <- struct{}{}
 }
 
 // End the operation.
 func (l *Limiter) End() {
-	l.cond.L.Lock()
-	l.count--
-	l.cond.Broadcast()
-	l.cond.L.Unlock()
+	<-l.sem
 }
